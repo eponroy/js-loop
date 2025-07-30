3b68d00169d5c798991342577d6c9f83656d045c
epon roy <eponroy16@gmail.com>
2025-07-30 07:26:49 +0600

    added for.js file

--- for.js
+++ for.js
@@ -1,1 +1,14 @@
-cont number = [12, 9];
+// const number = [12, 98, 45, 50, 30, 36, 40];
+
+// console.log(number);
+
+// for (const num of number) {
+//   console.log(num);
+// }
+
+const fruits = ["orange", "apple", "banana", "jackfruits", "watermelon"];
+
+for (const fruit of fruits) {
+  console.log(fruit);
+  console.log("I want to eat");
+}
